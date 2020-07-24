4b48272e0b07c798775a073e2a6c538fc3beae7c
Ty <68357354+jsxa@users.noreply.github.com>
2020-07-23 22:26:06 -0400

    Included CMath
    Changelog:
    Now times the multiple of 10 x 2.

--- main.cc
+++ main.cc
@@ -1,20 +1,18 @@
 // made by Tylerr, simple c++ console program
 // Edited on 07/22/2020
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
   int slices; //dec'd but not init'd
-  cout << "Yo dude how much pieces of pizza u got?: ";
+  cout << "Yo dumbass how much pieces of pizza u got?: ";
   cin >> slices;
   cout << "You have " << slices << " Slices of pizza" << endl;//returns the number of slices
+  cout << pow(10, 2);
   //<< = operator
   //cout = obj / console input out | cin = console input, ex: how many slices: {{inputhere}}
 }
 
-//int multiply(int x, int y) Commented out cause I will use this later.
-//{
-//    return x * y;
-//}
