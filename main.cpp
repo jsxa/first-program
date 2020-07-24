269d2cdd1cb8f923f9c52c591893c9d336032b8a
Ty <68357354+jsxa@users.noreply.github.com>
2020-07-23 22:22:15 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// made by Tylerr, simple c++ console program
+// Edited on 07/22/2020
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int slices; //dec'd but not init'd
+  cout << "Yo dude how much pieces of pizza u got?: ";
+  cin >> slices;
+  cout << "You have " << slices << " Slices of pizza" << endl;//returns the number of slices
+  //<< = operator
+  //cout = obj / console input out | cin = console input, ex: how many slices: {{inputhere}}
+}
+
+//int multiply(int x, int y) Commented out cause I will use this later.
+//{
+//    return x * y;
+//}
